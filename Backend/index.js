e7d78edf417a337be8979d3dc672e3ee868d01c8
MikeMike39 <Mfonseca294@gmail.com>
2021-08-13 10:06:41 -0400

    Done

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const newsRouter = require('./router/news');
+ 
+const morgan = require('morgan')
+app.use(morgan('dev'))
+
+app.use(express.static('public'));
+app.use(express.static('data/uploads'));
+app.use('/api', newsRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}.`);
+});
